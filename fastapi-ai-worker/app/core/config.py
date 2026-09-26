518de8d5d0914712171d9ad7d8d133f3413d4f46
UtkarshMudgal2802droid <utkarsh.mudgal123si@gmail.com>
2026-09-26 19:59:53 +0530

    feat(auth): implement dynamic JWT authentication and strict domain config

--- fastapi-ai-worker/app/core/config.py
+++ fastapi-ai-worker/app/core/config.py
@@ -4,8 +4,14 @@ class Settings(BaseSettings):
     PROJECT_NAME: str
     DATABASE_URL: str
     KAFKA_BROKER_URL: str
+    
+    # --- ADD THESE 3 NEW VARIABLES ---
+    JWT_SECRET_KEY: str
+    ADMIN_USERNAME: str
+    ADMIN_PASSWORD: str
 
     class Config:
         env_file = ".env"
+        extra = "ignore" 
 
 settings = Settings()
